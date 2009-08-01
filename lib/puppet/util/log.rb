7a708e61a2ad087f3a181bd7607f328efe455c20
Nigel Kersten <nigelk@google.com>
2009-07-31 17:36:50 -0700

    Fixes #2464, #2457. Deprecate reportserver for report_server. Add report_port setting. Add tests.

--- lib/puppet/util/log.rb
+++ lib/puppet/util/log.rb
@@ -514,7 +514,7 @@ class Puppet::Util::Log
         # We can't store the actual source, we just store the path.
         # We can't just check for whether it responds to :path, because
         # plenty of providers respond to that in their normal function.
-        if (source.is_a?(Puppet::Type) or source.is_a?(Puppet::Parameter)) and source.respond_to?(:path)
+        if defined?(Puppet::Type) and (source.is_a?(Puppet::Type) or source.is_a?(Puppet::Parameter)) and source.respond_to?(:path)
             @source = source.path
         else
             @source = source.to_s
